230357ac2c8d3a01399c9d30ca90efd0b5ac60ff
Bowen <94429531+Bowen1587@users.noreply.github.com>
2024-03-12 15:32:55 +0800

    Fixed LocalDateTime function

--- PacketFilterMerge.bat
+++ PacketFilterMerge.bat
@@ -4,8 +4,8 @@ setlocal enableDelayedExpansion
 ::Get LocalDateTime
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%_%ldt:~8,2%%ldt:~10,2%
-set date=%ldt:~0,4%%ldt:~5,2%%ldt:~8,2%
-set time=%ldt:~11,2%%ldt:~14,2%
+set date=%ldt:~0,8%
+set time=%ldt:~9,4%
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::Change directory to the desired folder
